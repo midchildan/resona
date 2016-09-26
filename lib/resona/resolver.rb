8d0b7c666d5ca5be0de1ff5b2014153dc25cc7a0
midchildan <midchildan+git@gmail.com>
2016-09-27 00:59:28 +0900

    Fix issue with multiple matching gem versions.
    A gem sometimes have multiple variations of the same version intended
    for different platforms. Before these changes, resona couldn't handle
    this case very well and returned the wrong checksum in some cases. But
    as it turned out, we were able to ask Bundler for the correct checksum
    of the gem we wanted.
    
    To sum up, these changes
    
      - Removes manual queries to rubygems.org for checksums
      - Asks Bundler for the checksums instead

--- lib/resona/resolver.rb
+++ lib/resona/resolver.rb
@@ -1,13 +1,7 @@
 require "bundler"
-require "cgi"
-require "net/http"
-require "uri"
-require "yaml"
 
 module Resona
   module Resolver
-    REMOTE_URI = "https://rubygems.org"
-
     class << self
       def run(gemfile_path, **options)
         check_group_conflicts(options[:with], options[:without])
@@ -18,26 +12,17 @@ module Resona
         definition = Bundler::Definition.build(gemfile_path, nil, nil)
         definition.resolve_remotely!
 
-        # For each gem, get a list of available versions.
-        gem_versions = {}
-        uri = URI(REMOTE_URI)
-        http = Net::HTTP.new(uri.host, uri.port)
-        http.use_ssl = true if uri.scheme == "https"
-        http.start do |h|
-          definition.specs.each do |spec|
-            gem_versions[spec.name] = query_gem_versions(h, spec.name)
-          end
-        end
-
-        # For each gem, find the matching version and its checksum.
+        # For each gem, extract the necessary version and its checksum.
         gems = definition.specs.inject({}) do |acc, spec|
+          next acc unless spec.is_a? Bundler::EndpointSpecification
+
           name = spec.name
           version = spec.version.to_s
-          matching = gem_versions[name].find { |v| v["number"] == version }
-          raise ChecksumNotFound.new(name, version) if matching.nil?
-          checksum = matching["sha"]
+          checksum = spec.checksum
+          remote_uri = spec.remote.anonymized_uri
 
-          acc[name] = { version: version, checksum: checksum }
+          acc[name] = { version: version, checksum: checksum,
+                        remote_uri: remote_uri }
           acc
         end
 
@@ -47,24 +32,11 @@ module Resona
 
       private
 
-      def query_gem_versions(remote_http, name)
-        response = remote_http.get("/api/v1/versions/#{CGI.escape(name)}.yaml")
-        response.value unless response.is_a? Net::HTTPSuccess
-
-        YAML.load(response.body)
-      end
-
       def check_group_conflicts(with, without)
         if with && without
           conflicting = with & without
           raise GroupConflict.new(conflicting) unless conflicting.empty?
         end
-      end
-    end
-
-    class ChecksumNotFound < StandardError
-      def initialize(gem_name, gem_version)
-        super("Failed to obtain checksum for #{gem_name}-#{gem_version}.")
       end
     end
 
